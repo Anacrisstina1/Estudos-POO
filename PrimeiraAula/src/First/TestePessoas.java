4b598f1a04c3bc6d044f8349aa05ac97edbaee36
Ana Cristina <anacristinadasilva1511@gmail.com>
2023-08-24 21:30:40 -0300

    iniciando estudo

---
+++ PrimeiraAula/src/First/TestePessoas.java
@@ -0,0 +1,23 @@
+package First;
+
+	import java.util.Scanner;
+
+	public class TestePessoas {
+	    public static void main(String[]args) {
+	        Scanner entra = new Scanner(System.in);
+	        Pessoas p = new Pessoas();
+	        
+	        System.out.println("Insira os dados.");
+	        System.out.println("Insira o id: ");
+	        p.id = entra.nextInt();
+	        entra.nextLine();
+	        System.out.println("Insira o nome: ");
+	        p.nome = entra.nextLine();
+	        System.out.println("Insira o endereÃ§o: ");
+	        p.endereco	= entra.nextLine();
+	        p.ImprimirPessoa();
+	        entra.close();
+	    }
+	}
+
+
